9b47ec55b29b1059478d7d889b7b71b3a50116ed
Justyn <justynmartinez419@gmail.com>
2024-09-28 13:41:15 -0400

    added support and fixed the items page, still have to tweak some styling on items but it works

--- reactFrontend/src/components/Homepage/HomePage.jsx
+++ reactFrontend/src/components/Homepage/HomePage.jsx
@@ -161,11 +161,6 @@ async function getCharacterImage(){
     return(
       <>
       
-    {/* <link rel="images" type="jpg" href="../Images/fflogo.jpg"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> */}
 
 <div className="homepagebody">
         
--- reactFrontend/src/components/Support/Support.jsx
+++ reactFrontend/src/components/Support/Support.jsx
@@ -1,11 +1,72 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Support.css'; 
+import logo from '../Images/fflogo.jpg'; 
 
-
-export default function Support(){
-    
-    
+export default function SupportPage() {
     return (
         <>
-        <h1>support test</h1>
+        <div className='supportbody'>
+        
+        <div>
+     <Link to="/"><img id="logo" src={logo} alt="fflogo"/></Link>
+        </div>
+
+    <div id="topnav">
+            <Link to={"/news"} className="navlinks">News</Link>
+            <Link to={"/profile"} className="navlinks">Profile</Link>
+            <Link to={"/items"} className="navlinks">Items</Link>
+            <Link to={"/support"} className="navlinks" >Support</Link>
+    </div>
+
+            <Container id="main">
+                <Row>
+                    <Col id="leftSide">
+                        <div id="formContainer">
+                            <Form id="contactForm">
+                                <h2 id="contactHeader">Contact us!</h2>
+                                <Form.Group controlId="emailInput" className='contactUserInput'>
+                                    <Form.Label>Email:</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        name="email"
+                                        placeholder="example@email.com"
+                                        required
+                                        className='InputField'
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="nameInput" className='contactUserInput'>
+                                    <Form.Label>Name:</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="name"
+                                        placeholder="John"
+                                        className='InputField'
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="messageInput" className='contactUserInput'>
+                                    <Form.Label>Message:</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        rows={10}
+                                        maxLength={500}
+                                        placeholder="How can we help?"
+                                        required
+                                        className='MessageInputField'
+                                        
+                                    />
+                                </Form.Group>
+                                <Button id="submitInput" type="submit" variant="primary">
+                                    Submit
+                                </Button>
+                            </Form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
         </>
-    )
-}
+    );
+};
